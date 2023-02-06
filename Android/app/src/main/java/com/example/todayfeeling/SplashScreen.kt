82d289e25fbf935b94129dd210ffdfc2f29c6d29
안성준 <anseongjun@anseongjuns-MacBook-Pro.local>
2023-02-06 14:06:51 +0900

    TF-28 스플래시화면 개발

---
+++ Android/app/src/main/java/com/example/todayfeeling/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.example.todayfeeling
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
