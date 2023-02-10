9eaba44f010cdf5b6c010abc3647c43bb8c5d86d
안성준 <anseongjun@172.20.nate.com>
2023-02-10 16:23:16 +0900

    TF-32--profile 프로필 화면 개발 및 자동로그인

--- Android/app/src/main/java/com/example/todayfeeling/MainActivity.kt
+++ Android/app/src/main/java/com/example/todayfeeling/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.todayfeeling
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.todayfeeling.databinding.ActivityMainBinding
 import com.example.todayfeeling.main.MainFragment
+import com.example.todayfeeling.profile.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -18,15 +20,15 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().add(R.id.body_container,MainFragment(), "main").commit()
+        supportFragmentManager.beginTransaction().add(R.id.body_container, MainFragment(), "main").commit()
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.main_page -> {
+                R.id.main -> {
                     changeFragment(MainFragment(), "main")
                 }
-                else -> {
-                    changeFragment(MainFragment(), "main")
+                R.id.profile -> {
+                    changeFragment(ProfileFragment(), "profile")
                 }
             }
             true
@@ -43,11 +45,14 @@ class MainActivity : AppCompatActivity() {
 
     fun updateBottomMenu(navigation: BottomNavigationView) {
         val tag1: Fragment? = supportFragmentManager.findFragmentByTag("main")
+        val tag2: Fragment? = supportFragmentManager.findFragmentByTag("profile")
 
         if(tag1 != null && tag1.isVisible) navigation.menu.findItem(R.id.main).isChecked = true
+        else if(tag2 != null && tag2.isVisible) navigation.menu.findItem(R.id.profile).isChecked = true
     }
 
     override fun onBackPressed() {
+        super.onBackPressed()
         val bnv = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
         updateBottomMenu(bnv)
     }
---
+++ Android/app/src/main/java/com/example/todayfeeling/profile/ProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.example.todayfeeling.profile
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todayfeeling.databinding.FragmentProfileBinding
+import com.example.todayfeeling.sign.LoginActivity
+
+class ProfileFragment : Fragment() {
+
+    private var mBinding: FragmentProfileBinding? = null
+
+    private val binding get() = mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        val sharedPreference = requireActivity().getSharedPreferences("user", 0)
+        sharedPreference.getString("id", "")
+        sharedPreference.getString("sex", "")
+
+        binding.btnLogout.setOnClickListener {
+            sharedPreference.edit().remove("id")
+            sharedPreference.edit().remove("pw")
+            sharedPreference.edit().remove("sex")
+            sharedPreference.edit().remove("session")
+            sharedPreference.edit().apply()
+            activity?.let{
+                val intent = Intent(context, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
