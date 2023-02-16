198b42d9ca7c35e4b95c349dc5cf117ff3eed69e
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2023-02-16 14:18:59 +0900

    TF-92 timestamp 추가, 통계 정보 조회 api 조정

--- Flask_server/app/run.py
+++ Flask_server/app/run.py
@@ -2,7 +2,7 @@ from create_app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    # app.run(host = '0.0.0.0', port = '10081')
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = '10081')
+    # app.run(debug=True)
 
 
